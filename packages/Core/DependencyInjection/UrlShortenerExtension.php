775dbd83c4666899f3d6c0e8282feca3fcbc802c
Tanguy Reviller <t.reviller@beapp.fr>
2019-03-12 14:14:37 +0100

    Init monorepo builder

--- packages/Core/DependencyInjection/UrlShortenerExtension.php
+++ packages/Core/DependencyInjection/UrlShortenerExtension.php
@@ -33,7 +33,7 @@ class UrlShortenerExtension extends Extension
         $configuration = new Configuration($container->getParameter('kernel.debug'));
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.yml');
 
